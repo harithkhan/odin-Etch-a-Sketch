7c956b43c046ff7c1f0a28fb0466579b502b84ce
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2024-12-07 23:46:10 +0800

    feat: add button to prompt number of grid boxes Add button that prompts the user to enter the number of grid boxes to input into each side of the grid. Add function to remove the existing grid structure and input the number of grid boxes as set by the use input in the prompt.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,34 @@
 const gridContainer = document.querySelector("#gridContainer");
-function createGrid () {
-    
-    for (let i = 0; i < 256; i++) {
+const gridButton = document.querySelector("#gridButton");
+
+function createGrid (gridNumber) {
+
+    gridContainer.innerHTML = "";
+
+    for (let i = 0; i < (gridNumber * gridNumber); i++) {
         const gridBox = document.createElement("div");
         gridBox.className = "gridBox";
         gridContainer.appendChild(gridBox);
     };
+
+    const allGridBox = document.querySelectorAll(".gridBox");
+    for (let i = 0; i < allGridBox.length; i++) {
+    allGridBox[i].addEventListener("mouseenter", toColorBox);
 };
-createGrid();
+};
 
 function toColorBox (event) {
     event.target.style.backgroundColor = "black";
 };
 
-const allGridBox = document.querySelectorAll(".gridBox");
-for (let i = 0; i < allGridBox.length; i++) {
-    allGridBox[i].addEventListener("mouseenter", toColorBox);
-};
+gridButton.addEventListener("click", () => {
+    let gridNumber = prompt("Please enter the number of grid boxes per side (between 0 and 101)", 10);
+    if (!isNaN(gridNumber) && gridNumber > 0 && gridNumber <= 100) {
+        createGrid(gridNumber);
+    } else {alert("Please enter a valid number between 0 and 101")}
+});
+
+createGrid(16);
+
+
+
